4829033dcf32cd321ddd6a291a2ccc25b183a647
dhrvchawla624-creator <dhruvchawla624@gmail.com>
2025-12-24 00:43:26 +0530

    fetch number

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.fke9tilh7f8"
+    "revision": "0.domd0somnao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
