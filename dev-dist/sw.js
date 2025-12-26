06dbdba2a5ff7fa93d457dd50c5b4af5456d0614
dhrvchawla624-creator <dhruvchawla624@gmail.com>
2025-12-27 02:22:09 +0530

    half light fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.domd0somnao"
+    "revision": "0.okdbcupkf3"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
